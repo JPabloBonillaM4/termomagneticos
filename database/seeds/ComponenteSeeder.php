55073d841a679ebbdac02ce0d8ee5eae085c1401
J Pablo <jpm_caprico@hotmail.com>
2020-10-14 23:48:33 -0500

    modifying seederComponents

--- database/seeds/ComponenteSeeder.php
+++ database/seeds/ComponenteSeeder.php
@@ -13,37 +13,37 @@ class ComponenteSeeder extends Seeder
     {
         Componente::create(array(
             'nombre'      => 'protección electromagnética',
-            'imagen'      => 'testImagen.jpg',
+            'imagen'      => 'images/componentes/proteccion_electromagnetica.jpeg',
             'descripcion' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         ));
 
         Componente::create(array(
             'nombre'      => "mcb's y equipos de control",
-            'imagen'      => 'testImagen.jpg',
+            'imagen'      => 'images/componentes/mcbs_y_equipo_de_control.jpg',
             'descripcion' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         ));
 
         Componente::create(array(
             'nombre'      => 'protección termomagnética',
-            'imagen'      => 'testImagen.jpg',
+            'imagen'      => 'images/componentes/proteccion_termomagnetica.jpg',
             'descripcion' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         ));
 
         Componente::create(array(
             'nombre'      => 'contactores y guardamotores',
-            'imagen'      => 'testImagen.jpg',
+            'imagen'      => 'images/componentes/contactores_y_guardamotores.png',
             'descripcion' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         ));
 
         Componente::create(array(
             'nombre'      => 'arrancadores para motores',
-            'imagen'      => 'testImagen.jpg',
+            'imagen'      => 'images/componentes/arrancadores_para_motores.jpg',
             'descripcion' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         ));
 
         Componente::create(array(
             'nombre'      => 'tableros de distribución alumbrado/contactos',
-            'imagen'      => 'testImagen.jpg',
+            'imagen'      => 'images/componentes/tableros_de_distribucion.jpg',
             'descripcion' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).'
         ));
     }
