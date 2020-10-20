29b3d036bbf679806c2fd3b2a0b09af5fa14c8cc
J Pablo <jpm_caprico@hotmail.com>
2020-10-19 20:42:32 -0500

    contact and blindaje images

--- resources/views/pages/contacto/index.blade.php
+++ resources/views/pages/contacto/index.blade.php
@@ -1,7 +1,7 @@
 @extends('index')
 @section('title','contacto')
 @section('content')
-<div class="parallax-medium section1">
+<div class="parallax-medium titulo_contacto">
     <div class="text-white h-100 d-flex align-items-center justify-content-center">
         <p class="text-uppercase display-4 font-weight-bold r2_mobile text-center">contacto</p>
     </div>
@@ -60,33 +60,38 @@
                     </div>
                 </div>
             </div>
-            <div class="row my-5" id="cotizacion">
-                <h3 class="text-center col-md-12 my-5 display-5">Cotización</h3>
-                <form action="{{ route('sendEmailCotizacion') }}" method="POST" class="col-md-8 m-auto">
-                    @csrf
-                    <div class="form-group">
-                        <label for="nombre">Nombre</label>
-                        <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="nameHelp" placeholder="Escriba su nombre" required>
-                        {{-- <small id="nameHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
-                    </div>
-                    <div class="form-group">
-                        <label for="telefono">Teléfono</label>
-                        <input type="tel" class="form-control onlyNumeric" maxlength="10" id="telefono" name="telefono" aria-describedby="telHelp" placeholder="Escriba su número de contacto" required>
-                        {{-- <small id="telHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Correo electrónico</label>
-                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Escriba su correo electrónico" required>
-                        {{-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
-                    </div>
-                    <div class="form-group">
-                        <label for="cotización">¿Qué desea cotizar?</label>
-                        <textarea class="form-control" id="cotizacion" name="cotizacion" rows="3" placeholder="Listado o descripción de su pedido" required></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary w-100">Enviar</button>
-                </form>
-            </div>
         </div>
+    </div>
+</div>
+<div class="row my-5 background_cotizacion background-simple" id="cotizacion">
+    <div class="background-shadow col-md-12 py-5 text-white">
+        <h3 class="text-center my-5 display-5">Cotización</h3>
+        <p class="text-center">
+            <i>Por favor introduce tus datos y te responderemos tan pronto nos sea posible</i>
+        </p>
+        <form action="{{ route('sendEmailCotizacion') }}" method="POST" class="col-md-8 m-auto pb-5">
+            @csrf
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="text" class="form-control" id="nombre" name="nombre" aria-describedby="nameHelp" placeholder="Escriba su nombre" required>
+                {{-- <small id="nameHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
+            </div>
+            <div class="form-group">
+                <label for="telefono">Teléfono</label>
+                <input type="tel" class="form-control onlyNumeric" maxlength="10" id="telefono" name="telefono" aria-describedby="telHelp" placeholder="Escriba su número de contacto" required>
+                {{-- <small id="telHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
+            </div>
+            <div class="form-group">
+                <label for="email">Correo electrónico</label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Escriba su correo electrónico" required>
+                {{-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> --}}
+            </div>
+            <div class="form-group">
+                <label for="cotización">¿Qué desea cotizar?</label>
+                <textarea class="form-control" id="cotizacion" name="cotizacion" rows="3" placeholder="Listado o descripción de su pedido" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary w-100 mt-3">Enviar</button>
+        </form>
     </div>
 </div>
 @endsection
