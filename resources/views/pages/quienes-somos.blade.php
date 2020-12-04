ce7a60a53b78117ae85d0e4b98945e85f98f18fd
J Pablo <juan@teknik.mx>
2020-12-04 17:29:44 -0600

    refixed finder img

--- resources/views/pages/quienes-somos.blade.php
+++ resources/views/pages/quienes-somos.blade.php
@@ -9,7 +9,7 @@
                 <img src="{{ asset('images/logos_inicio/ide.png') }}" class="mr-3 card-contact mt-2 img-fluid img-logo-index p-2" alt="logo ide">
                 <img src="{{ asset('images/logos_inicio/tdk.png') }}" class="mt-2 card-contact img-fluid img-logo-index p-2" alt="logo tdk">
                 <img src="{{ asset('images/logos_inicio/moeltek.png') }}" class="mr-3 card-contact mt-2 img-fluid img-logo-index p-2" alt="logo moeltek">
-                <img src="{{ asset('images/logos_inicio/finder.png') }}" class="card-contact mt-2 img-fluid img-logo-index" alt="logo finder">
+                <img src="{{ asset('images/logos_inicio/finder-cut.png') }}" class="card-contact mt-2 img-fluid img-logo-index" alt="logo finder">
             </div>
             <h1 class="title-our text-white text-uppercase text-center title adjust_title adjust_title_mobile container">termomagnéticos y control de radiaciones S.A de C.V</h1>
             {{-- <a data-scroll href="#quienes_somos" class="arrow_ancle">
